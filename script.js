ecbc1d9f533d8616060fde7fd52b33f546144665
Sean Kempf <poserposer@me.com>
2020-08-25 18:28:02 -0700

    Declared user input variables

--- script.js
+++ script.js
@@ -18,17 +18,34 @@ function writePassword() {
  */
 function generatePassword(){
 
+// Declaring the user inputs
+
+  let passwordLength = prompt("How many characters do you want your password? (8-128 characters)");
+  if (passwordLength < 8 || passwordLength > 128) {
+alert("Length needs to be 8-128 characters!")
+  }
+  let passwordUpper = confirm ("Would you like upper case letters? ");
+  let passwordLower = confirm ("Would you like lower case letters?");
+  let passwordSpecial = confirm ("Would you like special characters?");
+  let passwordNumbers = confirm ("Would you like numbers?")
+ console.log(passwordLength, passwordLower, passwordNumbers, passwordSpecial, passwordUpper)
   // Password is currently blank! We need to make a better one
-  let password = "";
+
+// let password = 
 
 
 
 
-
-  return password;
+  // return password;
 }
 
 // Main Process
 
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
+
+
+
+
+
+
